02e906db6886ceaaf50eff08576326397a88a52f
MFJcode <mfjcode@gmail.com>
2021-10-01 16:22:12 +0200

    Push the value from the input field

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
 
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
 })
