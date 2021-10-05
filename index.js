675989f250f99cf0eb3a4de13b86a972a463bc24
MFJcode <mfjcode@gmail.com>
2021-10-05 14:30:31 +0200

    Improving the performance of our app

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let listItems = " ";
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     console.log(myLeads);
 })
 
 for (let i = 0; i < myLeads.length; i++) {
-   //  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"; 
-
-   const li = document.createElement("li");
-    li.textContent = myLeads[]
+    listItems += "<li>" + myLeads[i] + "</li>"; 
 } 
+
+ulEl.innerHTML = listItems;
