37e5dfb92425dc2e1ec3e0cef4d7d832a965668b
MFJcode <mfjcode@gmail.com>
2021-10-11 16:28:13 +0200

    Make the delete button work

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") ); 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
 
+deleteBtn.addEventListener("dblclick", function () {
+
+    localStorage.clear()
+    myLeads = [];
+    renderLeads();
+
+})
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
     inputEl.value = '';
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) );
-    renderLeads() 
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads();
 })
 
 function renderLeads() {
@@ -30,8 +38,8 @@ function renderLeads() {
             </a>
         </li>
         `
-    } 
-    
+    }
+
     ulEl.innerHTML = listItems;
 }
 
