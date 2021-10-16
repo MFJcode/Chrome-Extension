7709b3453bb6ad65741df3e0a2d210994726056f
MFJcode <mfjcode@gmail.com>
2021-10-16 17:02:01 +0200

    Chrome API to get the tab

--- index.js
+++ index.js
@@ -13,16 +13,13 @@ if (leadsFromLocalStorage) {
     render(myLeads);
 }
 
-const tabs = [
-    {url: "https://github.com/MFJcode"}
-]
-
-
 tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     myLeads.push(tabs[0].url);
     localStorage.setItem("myLeads", JSON.stringify(myLeads) );
     render(myLeads); 
 
+   })
 }) 
 
 function render(leads) {
